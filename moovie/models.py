6382c888851ab39b0de1f601248cc59aaba7960e
Kai Li <2612733l@student.gla.ac.uk>
2022-02-19 16:18:18 +0000

    model test

--- moovie/models.py
+++ moovie/models.py
@@ -4,20 +4,20 @@ from django.contrib.auth.models import User
 
 class Movie(models.Model):
 
-    title = models.CharField(max_length=128, blank=False)
+    title = models.CharField(max_length=128, blank=False, unique=True)
     duration = models.IntegerField(default=0)
     release_date = models.DateTimeField(blank=False)
-    description = models.CharField(max_length=256)
+    description = models.TextField(max_length=256)
     average_rating = models.DecimalField(max_digits=3, decimal_places=2, default=0.00)
     image = models.ImageField(upload_to='movie_images', default = 'media/movie_images/movie_placeholder.png')
     create_date = models.DateTimeField(auto_now_add=True, blank=False)
     
     def __str__(self):
-        return self.title
+        return str(self.id) + '_' + self.title
 
 class Genre(models.Model):
 
-    name = models.CharField(max_length=32, primary_key=True)
+    name = models.CharField(max_length=32, primary_key=True, unique=True)
     slug = models.SlugField(unique=True)
 
     def save(self, *args, **kwargs):
@@ -35,7 +35,7 @@ class Person(models.Model):
     person_type = models.CharField(max_length=16, blank=False)
     
     def __str__(self):
-        return self.name + self.surname
+        return str(self.id) + '_' + self.name + ' ' + self.surname
 
 class DirectorMovie(models.Model):
 
@@ -52,39 +52,43 @@ class UserProfile(models.Model):
     BIO_MAX_LENGTH = 128
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    
+
     age = models.IntegerField(default=0)
-    bio = models.CharField(max_length=BIO_MAX_LENGTH)
-    picture = models.ImageField(upload_to='profile_images', blank=True)
+    bio = models.TextField(max_length=BIO_MAX_LENGTH, blank=True)
+    picture = models.ImageField(upload_to='profile_images', default='profile_images/placeholder.jpg')
    
     def __str__(self):
         return self.user.username
+
 
 class Review(models.Model):
 
     COMMENT_MAX_LENGTH = 512
     HEADER_MAX_LENGTH = 128
 
-    username = models.ForeignKey(User, to_field="username", on_delete=models.CASCADE)
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
     movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE)
 
-    comment = models.CharField(max_length=COMMENT_MAX_LENGTH)
+    comment = models.TextField(max_length=COMMENT_MAX_LENGTH)
     header = models.CharField(max_length=HEADER_MAX_LENGTH)
-    rating = models.DecimalField(max_digits=3, decimal_places=2)
+    rating = models.DecimalField(max_digits=3, decimal_places=2, null=True)
     date = models.DateTimeField(auto_now_add=True, blank=False)
     
     def __str__(self):
         return self.header
+
 
 class MovieToWatch(models.Model):
 
     username = models.ForeignKey(User, to_field="username", on_delete=models.CASCADE)
     movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE)
 
+
 class MovieGenre(models.Model):
 
     genre_name = models.ForeignKey(Genre, on_delete=models.CASCADE)
     movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
 
 class ContactMessage(models.Model):
 
--- populate_moovie.py
+++ populate_moovie.py
@@ -9,8 +9,47 @@ from django.core.files.images import ImageFile
 from decimal import *
 import datetime
 
+
 def populate():
-    add_movie('Harry Potter and the Philosophers Stone', 152, datetime.datetime(2001, 11, 16), 'First movie of the series.', Decimal(4.71), "movie_images/HarryPotter1.jpg")
+    # Add user test
+    user1 = add_user('user1', '12345678', 'example@gmail.com', 'Rogers', 'Choi')
+    user2 = add_user('user2', '87654321', 'example@gmail.com', 'Anne', 'Hathaway')
+    add_user_profile(user1, 25, 'profile_images/user1.jpg', 'my bio')
+    add_user_profile(user2, 25, 'profile_images/user1.jpg', '2 bio')
+
+    # Movie and actors test
+    movie1 = add_movie('Harry Potter and the Philosophers Stone', 152, datetime.datetime(2001, 11, 16), 'First movie of the series.', Decimal(4.71), "movie_images/HarryPotter1.jpg")
+    person = add_person("Daniel", 'Radcliffe', 'person_images/Daniel_Radcliffe.jpg', 'Actor')
+    person2 = add_person("Emma", "Watson", 'person_images/Emma_Watson.jpg', 'Actor')
+    person3 = add_person('Chris', 'Columbus', 'person_images/Chris_Columbus.jpg', 'Director')
+    add_actor_movie(movie1, person)
+    add_actor_movie(movie1, person2)
+    add_director_movie(movie1, person3)
+
+    movie2 = add_movie('Harry Potter and the Prisoner of Azkaban', 142, datetime.datetime(2004, 6, 4), 'Some description', Decimal(4.56), 'movie_images/HarryPotter3.jpg')
+    person4 = add_person('Alfonso', 'Cuarón', 'person_images/Alfonso_Cuarón.jpg', 'Director')
+    add_actor_movie(movie2, person)
+    add_actor_movie(movie2, person2)
+    add_director_movie(movie2, person4)
+
+    # user review test
+    add_review(user1, movie1, 'comment test', 'header test', Decimal(4.2))
+
+    # Movie to watch test
+    add_movie_to_watch(user1, movie1)
+    add_movie_to_watch(user2, movie1)
+
+    # MovieGenre test
+    adventure = add_genre('Adventure')
+    fantasy = add_genre('Fantasy')
+    add_movie_genre(movie1, adventure)
+    add_movie_genre(movie1, fantasy)
+    add_movie_genre(movie2, adventure)
+    add_movie_genre(movie2, fantasy)
+
+    # Contact message test
+    add_contact_message('example@gmail.com', 'Anonymous', 'test', 'test', datetime.datetime)
+
 
 
 def add_movie(title, duration, release_date, description, average_rating, image):
@@ -41,26 +80,30 @@ def add_actor_movie(movie_id, person_id):
     actor_movie.save()
     return actor_movie
 
-def add_user_profile(username, password, email, first_name, last_name, age, image, bio):
+
+def add_user(username, password, email, first_name, last_name):
     user = User.objects.get_or_create(username=username)[0]
+    user.set_password(password)
     user.email = email
     user.first_name = first_name
     user.last_name = last_name
-    user.set_password(password)
     user.save()
+    return user
 
-    user_profile = UserProfile.objects.create(user=user, age=age, image=image, bio=bio)
-    user_profile.save()
+
+def add_user_profile(user, age, picture, bio):
+    user_profile = UserProfile.objects.get_or_create(user=user, age=age, picture=picture, bio=bio)
     return user_profile
 
-def add_review(username, movie_id, comment, header, rating, date):
+
+def add_review(username, movie_id, comment, header, rating):
     review = Review.objects.get_or_create(username=username, movie_id=movie_id)[0]
     review.comment = comment
     review.header = header
     review.rating = rating
-    review.date = date
     review.save()
     return review
+
 
 def add_movie_to_watch(username, movie_id):
     movie_to_watch = MovieToWatch.objects.get_or_create(username=username, movie_id=movie_id)[0]

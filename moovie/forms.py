090fc55514bd29cc0d3120347b243fa566f44ecd
GuangyuBi <2539558b@student.gla.ac.uk>
2022-03-10 12:06:45 +0000

    Add footer of base.html

--- moovie/forms.py
+++ moovie/forms.py
@@ -4,8 +4,12 @@ from moovie.models import Review, ContactMessage, UserProfile
 
 
 class ReviewForm(forms.ModelForm):
-    header = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': "Please enter a header."}), max_length=Review.HEADER_MAX_LENGTH, help_text="Header")
-    comment = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control md-textarea', 'placeholder': "Please write your comment."}), max_length=Review.COMMENT_MAX_LENGTH, help_text="Comment")
+    header = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': "Please enter a header."}),
+        max_length=Review.HEADER_MAX_LENGTH, help_text="Header")
+    comment = forms.CharField(
+        widget=forms.Textarea(attrs={'class': 'form-control md-textarea', 'placeholder': "Please write your comment."}),
+        max_length=Review.COMMENT_MAX_LENGTH, help_text="Comment")
     rating = forms.IntegerField(max_value=5, min_value=1, initial=1, help_text="Rating")
 
     class Meta:
@@ -14,15 +18,20 @@ class ReviewForm(forms.ModelForm):
 
 
 class ContactMessageForm(forms.ModelForm):
-    sender_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': "Your Name *"}), max_length=ContactMessage.NAME_MAX_LENGTH, help_text="Full Name")
-    sender_email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': "Your Email *"}), max_length=ContactMessage.EMAIL_MAX_LENGTH, help_text="Email")
-    subject = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': "Subject *"}), max_length=ContactMessage.SUBJECT_MAX_LENGTH, help_text="Subject")
-    message = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control md-textarea', 'placeholder': "Type Your Message..."}), max_length=ContactMessage.MESSAGE_MAX_LENGTH)
+    sender_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': "Your Name *"}),
+                                  max_length=ContactMessage.NAME_MAX_LENGTH, help_text="Full Name")
+    sender_email = forms.EmailField(
+        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': "Your Email *"}),
+        max_length=ContactMessage.EMAIL_MAX_LENGTH, help_text="Email")
+    subject = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': "Subject *"}),
+                              max_length=ContactMessage.SUBJECT_MAX_LENGTH, help_text="Subject")
+    message = forms.CharField(
+        widget=forms.Textarea(attrs={'class': 'form-control md-textarea', 'placeholder': "Type Your Message..."}),
+        max_length=ContactMessage.MESSAGE_MAX_LENGTH)
 
     class Meta:
         model = ContactMessage
         exclude = ('date',)
-
 
 
 class UserForm(forms.ModelForm):
@@ -30,7 +39,7 @@ class UserForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password',)
+        fields = ('username', 'email', 'password', 'first_name', 'last_name')
 
 
 class UserProfileForm(forms.ModelForm):

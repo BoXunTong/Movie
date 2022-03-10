f635f4090d3bc66ac5c4ab0e5a61dc0f2850be44
BoXun Tong <2636175T@student.gla.ac.uk>
2022-03-10 10:30:19 +0000

    search complete

--- moovie/urls.py
+++ moovie/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
 
     path('', views.IndexView.as_view(), name='index'),
     path('about-us/', views.AboutUsView.as_view(), name='about-us'),
-    path('search_result/', views.show_search_result, name='show_search_result')
+    path('search_result/', views.show_search_result, name='show_search_result'),
     path('movie/<int:movie_id>/', views.show_movie_profile, name='show_movie_profile'),
     path('movie/<int:movie_id>/review/', views.add_review, name='add_review'),
     path('contact/', views.contact_us, name='contact_us'),
